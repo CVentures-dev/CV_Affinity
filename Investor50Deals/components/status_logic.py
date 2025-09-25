c0ff51c00ab21cf7bbb17364b1e9dda169621d1d
CVentures-dev <dev@cventures.vc>
2025-09-25 13:40:42 +0300

    Added US in the list of compatible countries

--- Investor50Deals/components/status_logic.py
+++ Investor50Deals/components/status_logic.py
@@ -64,12 +64,12 @@ def define_status(companyHQ, companyStage, industrySector, investor_name, compan
 
 
 def is_compatible_geo(companyHQ: str) -> bool:
-    # Get a list of all European countries
+    # Get a list of all European countries + US
     compatible_countries = {country.name for country in pycountry.countries if country.alpha_2 in [
         "AL", "AD", "AM", "AT", "AZ", "BY", "BE", "BA", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR",
         "GE", "DE", "GR", "HU", "IS", "IE", "IT", "KZ", "XK", "LV", "LI", "LT", "LU", "MT", "MD", "MC",
         "ME", "NL", "MK", "NO", "PL", "PT", "RO", "RU", "SM", "RS", "SK", "SI", "ES", "SE", "CH", "TR",
-        "UA", "GB", "VA", ""
+        "UA", "GB", "VA", "", "US"
     ]}
     compatible_countries.add("Israel") 
     compatible_countries.add("Europe") 
